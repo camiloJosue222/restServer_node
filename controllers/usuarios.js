289c5c230130b5da2598dce03e7521607ce312fe
camiloJosue222 <camilojosue222@gmail.com>
2022-04-21 16:39:47 -0500

    primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,48 @@
+const { response, request }= require('express');
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { g , lastname = 'S/N'} = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        g,
+        lastname
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const { id } = req.params;
+        
+    res.json({
+        msg: 'put API - controlador',
+        id
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete
+}
